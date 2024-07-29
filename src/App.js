cc1bcdf2f04e893c38bdd590d8a024ab3f56732e
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-07-29 21:01:58 +0700

    auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import './App.css';
-import { Routes,Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AboutUs from './pages/Aboutus';
-import ContactUs from './pages/ContactUs';
-import Product from './pages/product';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Dashboard/login';
+import "./App.css";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutUs from "./pages/Aboutus";
+import ContactUs from "./pages/ContactUs";
+import Product from "./pages/product";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Dashboard/login";
+import { useState } from "react";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const ProtectedRoutes = () => {
+    return !isLogin && !user ? <Navigate to="/login" /> : <Outlet />;
+  };
   return (
     <>
       <Routes>
-      <Route path='login' element={<Login/>}/>
-        <Route path='*' element={<Home/>}/>
-        <Route path='aboutus' element={<AboutUs/>}/>
-        <Route path='contactus' element={<ContactUs/>}/>
-        <Route path='products' element={<Product/>}/>
-        <Route path='dashboard/*' element={<Dashboard/>}/>
+        <Route path="*" element={<Login setIsLogin={setIsLogin} />} />
+        <Route path="home" element={<Home />} />
+        <Route path="aboutus" element={<AboutUs />} />
+        <Route path="contactus" element={<ContactUs />} />
+        <Route path="products" element={<Product />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="dashboard/*" element={<Dashboard />} />
+        </Route>
       </Routes>
     </>
-    
   );
 }
 
