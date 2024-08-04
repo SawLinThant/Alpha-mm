736ad8adcdbca6503d3d366cd7d84ee882163e66
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-08-04 22:49:52 +0700

    dashboard-ui

--- src/components/custom-table/index.js
+++ src/components/custom-table/index.js
@@ -6,9 +6,56 @@ import {
 import "../../style/customtable.css";
 import { createColumnHelper } from "@tanstack/react-table";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useMemo } from "react";
+import { GrNext,GrPrevious } from "react-icons/gr";
+import { FaRegEdit } from "react-icons/fa";
 
-const CustomTable = ({data }) => {
+const CustomTable = ({ data }) => {
   const navigate = useNavigate();
+  const [pagination, setPagination] = useState(1);
+  const itemsPerPage = 3;
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const currentItems = useMemo(() => {
+    const start = (pagination - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    return data.slice(start, end);
+  }, [data, pagination, itemsPerPage]);
+
+  const handlePageChange = (direction) => {
+    console.log(direction)
+    setPagination((prevPagination) => {
+      const currentPage = prevPagination;
+      const newPage = direction === "next" ? prevPagination + 1 : prevPagination - 1;
+      if (newPage < 1 || newPage > totalPages) {
+        return currentPage; // Do not change the page if out of bounds
+      }
+      return newPage;
+      
+    });
+  };
+  const handlePageChangeByNumber = (pageNumber) => {
+      setPagination(pageNumber)
+  }
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <button
+          key={i}
+          className={`pagination-button ${
+            pagination === i ? "active" : ""
+          }`}
+          onClick={() => handlePageChangeByNumber(i)}
+        >
+          {i}
+        </button>
+      );
+    }
+    return pageNumbers;
+  };
+
   const columnHelper = createColumnHelper();
   const column = [
     columnHelper.accessor("image_url", {
@@ -39,30 +86,36 @@ const CustomTable = ({data }) => {
     columnHelper.accessor("id", {
       cell: (info) => (
         <button
+        style={{display:'flex', width:'100%', flexDirection:'row', alignItems:'center', justifyContent:'center', gap:'10px', border:'none', backgroundColor:'transparent', cursor:'pointer'}}
           onClick={() => {
             navigate(`/dashboard/detail/${info.getValue()}`);
           }}
         >
-          Detail
+         <p>Detail</p> <FaRegEdit />
         </button>
       ),
-      header: () => <span className="column-head">Price</span>,
+      header: () => <span className="column-head">Detail</span>,
     }),
   ];
 
   const table = useReactTable({
-    data: data,
+    data: currentItems,
     columns: column,
     getCoreRowModel: getCoreRowModel(),
   });
   return (
     <div className="custom-table-container">
+      <div className="custom-table-layout">
       <table className="custom-table">
         <thead className="table-head">
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id} className="header-row">
               {headerGroup.headers.map((header) => (
-                <th key={header.id} scope="col" className="column-head-container">
+                <th
+                  key={header.id}
+                  scope="col"
+                  className="column-head-container"
+                >
                   {header.placeholderId
                     ? null
                     : flexRender(
@@ -75,8 +128,8 @@ const CustomTable = ({data }) => {
           ))}
         </thead>
         <tbody>
-          {table.getRowModel()?.rows?.map((row) => (
-            <tr key={row.id} className="role-container">
+          {table.getRowModel()?.rows?.map((row,index) => (
+            <tr key={row.id} className={`data-row ${index % 2 === 0? "row-background-white":"row-background-gray"}`}>
               {row.getVisibleCells().map((cell) => (
                 <td key={cell.id} className="table-data">
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
@@ -86,6 +139,16 @@ const CustomTable = ({data }) => {
           ))}
         </tbody>
       </table>
+      </div>
+      
+      <div className="pagination-control-container">
+        <div className="pagination-control-layout">
+          <button className="prev-btn" disabled={pagination=== 1} onClick={() => handlePageChange('prev')}><GrPrevious /></button>
+          {renderPageNumbers()} {/* <div className="pagination-indicator">{pagination}<span>of</span>{totalPages}</div> */}
+          <button className="next-btn" disabled={pagination === totalPages} onClick={() => handlePageChange('next')}><GrNext /></button>
+          
+        </div>
+      </div>
     </div>
   );
 };
