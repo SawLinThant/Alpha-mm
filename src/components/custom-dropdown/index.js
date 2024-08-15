b5f90867cf88cc18112c9ab605e442afdc1ba3ed
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-08-15 20:17:21 +0700

    make-filter-dynamic

--- src/components/custom-dropdown/index.js
+++ src/components/custom-dropdown/index.js
@@ -1,9 +1,11 @@
 import { useState, useRef } from "react";
 import "./index.css";
 import { FaSortDown } from "react-icons/fa";
+import { GET_CATEGORIES_WITHOUT_SUB } from "../../graphql/queries/productQueries";
+import { useQuery } from "@apollo/client";
 
 const CustomDropdownFilter = ({ setCategory }) => {
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const selectRef = useRef(null);
 
@@ -17,19 +19,40 @@ const CustomDropdownFilter = ({ setCategory }) => {
     setIsOpen(!isOpen);
   };
 
+  const { data: category, loading } = useQuery(GET_CATEGORIES_WITHOUT_SUB, {
+    pollInterval: 500,
+  });
+  const categories = category ? category.category : [];
+  console.log("filter category:", categories);
+
   const options = [
-    { value: 'all', label: 'All Category' },
-    { value: 'kitchen', label: 'Kitchen' },
-    { value: 'laundry', label: 'Laundry' },
-    { value: 'entertainment', label: 'Entertainment' },
-    { value: 'miscellaneous', label: 'Miscellaneous' }
+    { value: "all", label: "All Category" },
+    ...categories.map((category) => ({
+      value: category.category_name,
+      label: category.category_name
+        .split(" ")
+        .map(
+          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(" "),
+    })),
   ];
 
   return (
-    <div className="custom-dropdown-container" onClick={toggleDropdown} ref={selectRef}>
+    <div
+      className="custom-dropdown-container"
+      onClick={toggleDropdown}
+      ref={selectRef}
+    >
       <div className="filter-select">
-        <p>{options.find(opt => opt.value === filter)?.label || 'Select Category'}</p>
-        <div><FaSortDown /></div></div>
+        <p>
+          {options.find((opt) => opt.value === filter)?.label ||
+            "Select Category"}
+        </p>
+        <div>
+          <FaSortDown />
+        </div>
+      </div>
       {isOpen && (
         <div className="dropdown-options">
           {options.map((option) => (
@@ -48,4 +71,3 @@ const CustomDropdownFilter = ({ setCategory }) => {
 };
 
 export default CustomDropdownFilter;
-
