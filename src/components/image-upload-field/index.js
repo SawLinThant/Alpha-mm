471b6feca80ebc90715c023d43198ae797253683
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-08-16 13:57:14 +0700

    ui-update

--- src/components/image-upload-field/index.js
+++ src/components/image-upload-field/index.js
@@ -24,7 +24,7 @@ const ImageUploadField = ({ handleImageChange, imageUrl, image , label,fontsize,
             <img
             src={imageUrl}
             alt="Uploaded"
-            style={{ width: "350px", height: "100%" }}
+            style={{ width: "100%", height: "100%" }}
           />
         ):
         (<div className="file-upload-image-icon-container">
